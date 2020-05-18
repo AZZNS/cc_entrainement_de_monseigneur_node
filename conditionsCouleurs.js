677d8be026f19305ee50e58823a6fd88fefd92eb
AZZNS <semet.clement@gmail.com>
2020-05-18 11:51:20 +0200

    Create conditionsCouleurs.js

---
+++ conditionsCouleurs.js
@@ -0,0 +1,20 @@
+let couleur = "rouge";
+if (couleur == "rouge") {
+	console.log("la couleur est rouge")
+}
+
+else if (couleur == "bleu") {
+	console.log("la couleur est bleu")
+}
+
+else if (couleur == "jaune") {
+	console.log("la couleur est jaune")
+}
+
+else if (couleur == "vert") {
+	console.log("la couleur est vert")
+}
+
+else {
+	console.log("La couleur était ${couleur});
+}
